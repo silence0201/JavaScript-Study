1ce22528fdc6170d8ca0dc805b38906c9817b17b
Silence <374619540@qq.com>
2017-04-05 17:01:28 +0800

    generator

---
+++ Simple/generator.js
@@ -0,0 +1,33 @@
+/**
+ * Created by silence on 2017/4/5.
+ */
+
+function* foo(x) {
+    yield x + 1;
+    yield x + 2;
+    return x + 3;
+}
+
+function* fib(max) {
+    var
+        t,
+        a = 0,
+        b = 1,
+        n = 1;
+    while (n < max) {
+        yield a;
+        t = a + b;
+        a = b;
+        b = t;
+        n ++;
+    }
+    return a;
+}
+
+var f = fib(5);
+f.next(); // {value: 0, done: false}
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 2, done: false}
+f.next(); // {value: 3, done: true}
+
