8edc50e1fcdeee1b293a3c013952554b1f0fa089
Silence <374619540@qq.com>
2017-04-07 14:52:41 +0800

    Size

---
+++ ReactNative/size.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,View
+} from 'react-native';
+
+
+
+export default class AwesomeProject extends Component {
+  render() {
+      return (
+          <View>
+              <View style={{width: 50, height: 50,backgroundColor: 'powderblue'}} />
+              <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+              <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+          </View>
+      ) ;
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
