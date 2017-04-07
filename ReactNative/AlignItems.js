b9147eb1b7cbc88300dc63c0236b73c80742d2c4
Silence <374619540@qq.com>
2017-04-07 15:48:30 +0800

    AlignItemsBasics

---
+++ ReactNative/AlignItems.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,View
+} from 'react-native';
+
+
+class AlignItemsBasics extends Component {
+    render() {
+        return (
+            // 尝试把`alignItems`改为`flex-start`看看
+            // 尝试把`justifyContent`改为`flex-end`看看
+            // 尝试把`flexDirection`改为`row`看看
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    } ;
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
