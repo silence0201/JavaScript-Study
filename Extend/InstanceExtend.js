828c086b8f02632c6c94f2df74aa170d35be199e
Silence <374619540@qq.com>
2018-05-14 20:50:34 +0800

    instanceExtend

---
+++ Extend/InstanceExtend.js
@@ -0,0 +1,27 @@
+// 定义一个动物类
+function Animal() {
+    // 属性
+    this.name = 'Animal';
+    // 实例方法
+    this.sleep = function () {
+        console.log(this.name + '正在睡觉！');
+    }
+}
+
+// 原型方法
+Animal.prototype.eat = function (food) {
+    console.log(this.name + '正在吃：' + food);
+};
+
+function Cat(name) {
+    var instance = new Animal();
+    instance.name = 'Tom';
+    return instance;
+}
+
+// Test Code
+var cat = new Cat();
+console.log(cat.name);
+console.log(cat.sleep());
+console.log(cat instanceof Animal); // true
+console.log(cat instanceof Cat); // false
